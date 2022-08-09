b9ec49abe8ae4b760d339d8a4c0290b20629dd56
Mehdi Rezaii <99mehdirezaii99@gmail.com>
2022-08-09 23:23:14 +0430

    Finished Single Country

--- src/pages/SingleCountry.jsx
+++ src/pages/SingleCountry.jsx
@@ -6,13 +6,13 @@ const SingleCountry = () => {
   const path = useLocation();
   //   console.log(s.pathname.replace('/',''));
   let dispatch = useDispatch();
-  let select = useSelector((state) => state); 
+  let select = useSelector((state) => state);
   useEffect(() => {
     dispatch({
       type: "SAGA_GET_SINGLE_COUNTRY",
       peyload: path.pathname.replace("/", ""),
     });
-    console.log(select); 
+    console.log(select);
     return () => {
       dispatch({ type: "EMPTY_LIST" });
     };
@@ -24,14 +24,48 @@ const SingleCountry = () => {
     },
     [select]
   );
-  return ( 
-    <div className="container mx-auto mt-5">
+  return (
+    <div className="container pt-5 mx-auto mt-5 h-100">
       {select.countriesReducer.length !== 1 ? (
-        "Loading ..."
+        <div className="grid columns-1 text-center">Loading ...</div>
       ) : (
-        <div className="grid md:grid-cols-2 sm:grid-cols-1">
-            <div><img className="object-cover w-96" src={select.countriesReducer[0].flags.png} alt="" /></div>
-            <div className="text-right">s</div>
+        <div className="grid md:grid-cols-2  content-center  gap-5 sm:grid-cols-1">
+          <div>
+            <img
+              className="object-cover w-96"
+              src={select.countriesReducer[0].flags.png}
+              alt=""
+            />
+          </div>
+          <div className="text-left grid grid-cols-1 items-center h-100">
+            <p>
+              <b>name :</b>{" "}
+              <span className="text-gray-500">
+                {select.countriesReducer[0].name}
+              </span>
+            </p>
+            {/*  */}
+            <p>
+              <b>capital :</b>{" "}
+              <span className="text-gray-500">
+                {select.countriesReducer[0].capital}
+              </span>
+            </p>
+            {/*  */}
+            <p>
+              <b>population :</b>{" "}
+              <span className="text-gray-500">
+                {select.countriesReducer[0].population}
+              </span>
+            </p>
+            {/*  */}
+            <p>
+              <b>region :</b>{" "}
+              <span className="text-gray-500">
+                {select.countriesReducer[0].region}
+              </span>
+            </p>
+          </div>
         </div>
       )}
     </div>
