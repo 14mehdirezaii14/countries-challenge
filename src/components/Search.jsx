24214708f00a46ebb3ffdd4d78ed4377519bd7fb
Mehdi Rezaii <99mehdirezaii99@gmail.com>
2022-08-16 20:08:25 +0430

    add useMemo in Search Component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,16 @@
 import { useDispatch } from "react-redux";
-
+import { useMemo, useState } from "react";
 const Search = () => {
   const dispatch = useDispatch();
+  const [valueInput, setValueInput] = useState("");
+  useMemo(() => {
+    dispatch({ type: "SAGA_SEARCH_COUNTRY", peyload: valueInput });
+  }, [valueInput]);
   return (
     <div className="col-span-1 mb-5">
       <input
-        onChange={(e) => dispatch({ type: "SAGA_SEARCH_COUNTRY", peyload: e.target.value })}
+        value={valueInput}
+        onChange={(e) => setValueInput(e.target.value)}
         type="text"
         placeholder="Search Country"
         className="inputSearch"
